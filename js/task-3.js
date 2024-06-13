74cb2cf4d4f91ac81a51674a0358f66fa3761daa
unknown <you@example.com>
2024-06-13 13:55:14 +0300

    update hw3

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,13 @@
 function filterArray(numbers, value) {
-    const result = [];
-    for (let x = 0; x < numbers; x++) {
-      if (numbers[x] > value) {
-        result.push(numbers[x]);
-      }
+  const result = [];
+  for(let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      result.push(numbers[i]);
     }
-    return result;
   }
-  console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+  return result;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
 console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
